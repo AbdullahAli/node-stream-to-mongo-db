fa655954b155fa68bab71b157ca34595cbf0c23d
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2016-06-13 22:33:38 +0100

    WIP : inital commit

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import { Writable    } from 'stream';
+import Bluebird        from 'bluebird';
+
+var MongoClient = require("mongodb");
+Bluebird.promisifyAll(MongoClient);
+
+let config = {};
+let conn   = {};
+let batch  = [];
+
+let insertOptions = { w : 1};
+
+function streamToMongoDB(options) {
+    config = options;
+
+    if(!config.batchSize) {
+        config.batchSize = 1;
+    }
+
+    let writer = writableStream();
+
+    return writer;
+}
+
+function connect() {
+    return new Bluebird((resolve, reject) => {
+        MongoClient.connectAsync(config.dbURL)
+            .then(db => {
+                conn.db = db;
+                conn.collection = conn.db.collection(config.collection);
+                resolve();
+            })
+            .catch(error => reject(error));
+    });
+}
+
+function insertToMongo(records) {
+    return new Bluebird((resolve, reject) => {
+        conn.collection.insertAsync(records, insertOptions)
+            .then(resolve)
+            .catch(error => reject(error));
+    });
+}
+
+function insert(record) {
+    return new Bluebird(resolve => {
+        batch.push(record.person);
+
+        if(batch.length === config.batchSize) {
+            insertToMongo(batch)
+                .then(() => {
+                    batch = [];
+                    resolve();
+                });
+        } else {
+            resolve();
+        }
+    });
+}
+
+function writableStream() {
+    let writableStream = new Writable({
+        objectMode: true,
+        write: function(record, encoding, next) {
+            if(conn.db) {
+                insert(record).then(next);
+            } else {
+                connect().then(() => {
+                    insert(record).then(next);
+                });
+            }
+        }
+    });
+
+    writableStream.on('finish', () => {
+        // insert remainder of the batch that did not fit into the batchSize
+        insertToMongo(batch).then(() => {
+            console.log("ready to close..");
+            conn.db.close();
+        });
+    });
+
+    return writableStream;
+}
+
+module.exports = {
+    streamToMongoDB
+};
---
+++ src/tester.js
@@ -0,0 +1,34 @@
+import streamToMongoDB from "./index";
+import { MongoClient } from "mongodb";
+
+
+
+
+let config = {
+    dbURL: 'mongodb://localhost:27017/match',
+    collection: 'hmm',
+    batchSize: 20000
+};
+
+let streamer = streamToMongoDB.streamToMongoDB(config);
+
+MongoClient.connect(config.dbURL, function(err, db) {
+    // console.log("Connected correctly to server");
+
+    var col = db.collection('person_3_0_0');
+
+    var cursor = col.find({}).stream();
+    //
+    // console.log("will pipe now...");
+    cursor.pipe(streamer);
+
+    cursor.once("finish", () => {
+        console.log("camelcase");
+        db.close();
+    });
+
+    cursor.once("end", () => {
+        console.log("camelcase");
+        db.close();
+    });
+});
