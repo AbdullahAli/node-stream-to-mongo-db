12ee0b08b7c535667296c9b8b68e680512c90d16
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2016-06-14 21:09:42 +0100

    add gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import gulp from "gulp";
+import shell from "gulp-shell";
+
+gulp.task("watch", watch);
+
+function watch() {
+    return gulp.src("").pipe(shell([
+        "babel --watch src/ --out-dir out/"
+    ]));
+}
